2a9e570604cbe58e1f3082181de9b1f18895f501
Ganes yk <ganesyudhakusuma@gmail.com>
2024-09-13 11:06:35 +0700

    initial commit

---
+++ application/views/base/header.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+   <meta charset="utf-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+   <title><?= $title ?></title>
+   <link rel="stylesheet" href="<?= base_url('assets/vendors') ?>/simple-line-icons/css/simple-line-icons.css">
+   <link rel="stylesheet" href="<?= base_url('assets/vendors') ?>/flag-icon-css/css/flag-icons.min.css">
+   <link rel="stylesheet" href="<?= base_url('assets/vendors') ?>/css/vendor.bundle.base.css">
+   <link rel="stylesheet" href="<?= base_url('assets/vendors') ?>/font-awesome/css/font-awesome.min.css" />
+   <link rel="stylesheet" href="<?= base_url('assets/vendors') ?>/bootstrap-datepicker/bootstrap-datepicker.min.css">
+   <link rel="stylesheet" href="<?= base_url('assets/vendors') ?>/jvectormap/jquery-jvectormap.css">
+   <link rel="stylesheet" href="<?= base_url('assets/vendors') ?>/daterangepicker/daterangepicker.css">
+   <link rel="stylesheet" href="<?= base_url('assets/vendors') ?>/chartist/chartist.min.css">
+   <link rel="stylesheet" href="<?= base_url('assets') ?>/css/vertical-light-layout/style.css">
+   <link rel="shortcut icon" href="<?= base_url('assets') ?>/images/favicon.png" />
+</head>
+
+<body>
+   <script src="<?= base_url('assets/vendors') ?>/js/vendor.bundle.base.js"></script>
+   <script src="<?= base_url('assets/vendors') ?>/chart.js/chart.umd.js"></script>
+   <script src="<?= base_url('assets/vendors') ?>/jvectormap/jquery-jvectormap.min.js"></script>
+   <script src="<?= base_url('assets/vendors') ?>/jvectormap/jquery-jvectormap-world-mill-en.js"></script>
+   <script src="<?= base_url('assets/vendors') ?>/bootstrap-datepicker/bootstrap-datepicker.min.js"></script>
+   <script src="<?= base_url('assets/vendors') ?>/moment/moment.min.js"></script>
+   <script src="<?= base_url('assets/vendors') ?>/daterangepicker/daterangepicker.js"></script>
+   <script src="<?= base_url('assets/vendors') ?>/chartist/chartist.min.js"></script>
+   <script src="<?= base_url('assets/vendors') ?>/progressbar.js/progressbar.min.js"></script>
+   <script src="<?= base_url('assets') ?>/js/jquery.cookie.js"></script>
+   <script src="<?= base_url('assets') ?>/js/off-canvas.js"></script>
+   <script src="<?= base_url('assets') ?>/js/hoverable-collapse.js"></script>
+   <script src="<?= base_url('assets') ?>/js/misc.js"></script>
+   <script src="<?= base_url('assets') ?>/js/settings.js"></script>
+   <script src="<?= base_url('assets') ?>/js/todolist.js"></script>
+   <script src="<?= base_url('assets') ?>/js/dashboard.js"></script>
